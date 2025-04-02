b23c5856506334400fc122c51bc6f01d2e2868c9
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-04-02 12:17:31 +0530

    all about the routing in next

---
+++ demo/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About This section and more</div>
+  )
+}
+
+export default About
---
+++ demo/src/app/contact/user1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>This is rajib from the user</div>
+  )
+}
+
+export default User
